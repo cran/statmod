c3a07bc9e5fbef9f610c5b79fd67017d7448351d
Gordon Smyth <smyth@wehi.edu.au>
2004-03-20 00:00:00 +0000

    version 1.0.6

---
+++ tests/statmod-Tests.R
@@ -0,0 +1,23 @@
+library(statmod)
+
+set.seed(0); u <- runif(100)
+
+### glmgam.fit
+
+glmgam.fit(1,1)
+glmgam.fit(c(1,1),c(0,4))
+glmgam.fit(X=cbind(1,c(1,0.5,0.5,0,0)),y=rchisq(5,df=1))
+
+### randomizedBlock
+
+y <- rnorm(6)
+x <- rnorm(6)
+z <- c(1,1,2,2,3,3)
+randomizedBlock(y~x,random=z)
+
+### randomizedBlockFit
+
+y <- c(-1,1,-2,2,0.5)
+X <- matrix(1,5,1)
+Z <- model.matrix(~factor(c(1,1,2,2,3))-1)
+randomizedBlockFit(y,X,Z)
